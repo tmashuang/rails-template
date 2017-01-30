c9d7937c2aed491d13492b6ecd6c6d1be461e983
Thomas Huang <thomas.b.huang@gmail.com>
2017-01-30 07:26:57 -0800

    Rails generated test models

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,15 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save user without name" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "should not save user without name" do
+    user = User.new
+    assert_not user.save
+  end
+
 end
