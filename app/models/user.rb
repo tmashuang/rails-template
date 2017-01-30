3b6c570673ead545f13515d601ffb748bc666797
Thomas Huang <thomas.b.huang@gmail.com>
2017-01-30 07:26:31 -0800

    add bcrypt methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,30 @@
 class User < ApplicationRecord
+
+  validates :name, presence: true
+  validates :password_hash, presence: true
+  validates :karma, numericality: true
+  validates :email, presence: true, uniqueness: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by(email: email)
+    if user
+      if user.password == password
+        return user
+      else
+        return nil
+      end
+    else
+      return user
+    end
+  end
+
 end
